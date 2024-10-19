7df836e5162262ac3fb371fd89c1dd395acf1359
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-10-19 14:44:47 -0400

    Create read function

---
+++ aep-challange/aep_challange/utils/read_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+
+# Read the CSV file from the files directory
+def read_aep_csv() -> pd.DataFrame:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(script_dir, "../../../files/data1.csv")
+
+    # Read the CSV file
+    return pd.read_csv(csv_path)
+
+
+def read_pk(file_path: str) -> pd.DataFrame:
+    return pd.read_pickle(file_path)
+
+
+if __name__ == "__main__":
+    df = read_aep_csv()
